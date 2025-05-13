f7835005cff27466d541d81d88ecac0a9c7c30aa
stephansama <stephanrandle@hotmail.com>
2025-05-13 08:01:34 -0400

    feat: changed asciinema to replace in place

--- packages/remark-asciinema/src/asciinema.ts
+++ packages/remark-asciinema/src/asciinema.ts
@@ -22,17 +22,23 @@ export default function asciinema(
 	return function transformer(tree: Parameters<typeof visit>[0]) {
 		visit(
 			tree,
-			"text",
-			(node: Parameters<Visitor>[0] & { value: string }) => {
-				const { value } = node;
-				const asciinemaURL = getURL(value, (url) =>
+			"link",
+			(
+				node: Parameters<Visitor>[0] & { url: string },
+				index: Parameters<Visitor>[1],
+				parent: Parameters<Visitor>[2],
+			) => {
+				const asciinemaURL = getURL(node?.url, (url) =>
 					url.host.includes("asciinema.org"),
 				);
 
 				if (!asciinemaURL) return;
 
-				node.type = "html";
-				node.value = transform(asciinemaURL.toString());
+				// replace the node in place
+				parent?.children.splice(index!, 1, {
+					type: "html",
+					value: transform(asciinemaURL.toString()),
+				} as (typeof parent)["children"][number] & { value: string });
 			},
 		);
 	};
