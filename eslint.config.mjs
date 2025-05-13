025c1a0e1e52eefc20079f1a58eb6fe90517c033
stephansama <stephanrandle@hotmail.com>
2025-05-13 08:01:25 -0400

    chore: updated eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
 import pluginJs from "@eslint/js";
+import gitignore from "eslint-config-flat-gitignore";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+	gitignore(),
 	{ files: ["**/*.{js,mjs,cjs,ts}"] },
 	{ languageOptions: { globals: { ...globals.browser, ...globals.node } } },
 	pluginJs.configs.recommended,
